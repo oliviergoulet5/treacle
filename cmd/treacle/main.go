a98a89ce1d48ed8a0a89ee3bc96e41eebbdc74a7
Olivier Goulet <olivier_goulet@icloud.com>
2026-07-01 10:02:28 -0400

    feat: request route

--- cmd/treacle/main.go
+++ cmd/treacle/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -12,7 +14,8 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/health", healthHandler)
+	mux.HandleFunc("GET /health", healthHandler)
+	mux.HandleFunc("POST /request", requestHandler)
 
 	server := &http.Server{
 		Addr: ":" + port,
@@ -42,4 +45,61 @@ func getEnv(key, fallback string) string {
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("ok"))
+}
+
+type ExecuteRequest struct {
+	Method string `json:"method"`
+	URL string `json:"url"`
+}
+
+type ExecuteRequestResponse struct {
+	StatusCode int `json:"statusCode"`
+	Body string `json:"body"`
+}
+
+// Request handler for POST /request
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	// Read body
+	var executeRequest ExecuteRequest
+	err := json.NewDecoder(r.Body).Decode(&executeRequest)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	
+	// TODO: Sanitize executeRequest.Method
+	req, err := http.NewRequest(executeRequest.Method, executeRequest.URL, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadGateway)
+		return
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Failed %v", err)
+		http.Error(w, err.Error(), http.StatusBadGateway)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	executeRequestRes := ExecuteRequestResponse{
+		StatusCode: resp.StatusCode,
+		Body:       string(body),
+	}
+
+	// Send response
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(executeRequestRes); err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
